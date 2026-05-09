75c97bf999facf6ec547e2375cf396332cbf7fc4
Sachin dave <davesachin26@gmail.com>
2026-05-10 01:20:55 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {        
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int price : prices){            
+            if(minPrice > price){
+                minPrice = price;
+            }
+            else if(price - minPrice > maxProfit){
+                maxProfit = price - minPrice;
+            }           
+        }
+        return maxProfit; 
+    }
+}
